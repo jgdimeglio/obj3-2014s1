7d04067cb211c74600385190f19a1364b3500d64
José Di Meglio <jgdimeglio@gmail.com>
2014-04-13 00:10:48 +0200

    cambiando caracteristica a trait

--- complejo-deportivo/src/main/java/Mixins/Caracteristica.scala
+++ complejo-deportivo/src/main/java/Mixins/Caracteristica.scala
@@ -3,7 +3,7 @@ package Mixins
 import Clases.Cancha
 import org.joda.time.DateTime
 
-abstract class Caracteristica extends Cancha{
+trait Caracteristica extends Cancha{
 
   override 
 	def calcularExtra(dia : DateTime, inicio : Int, fin : Int, pb : Double) = (this.extraPorCaracteristica(pb) + 
