98caf6926830f12758e19f51f9d42d9cc2e75aae
José Di Meglio <jgdimeglio@gmail.com>
2014-04-12 22:26:17 +0200

    agregando métodos abstractos

--- complejo-deportivo/src/main/java/Clases/Cancha.scala
+++ complejo-deportivo/src/main/java/Clases/Cancha.scala
@@ -5,39 +5,37 @@ import Excepciones.YaEstaReservada
 import org.joda.time.DateTime
 import Excepciones.NoTieneIluminacion
 
-class Cancha() {
+abstract class Cancha() {
 	val reservas = new ArrayBuffer[Reserva]
 	
 	
 	def reservar(dia : DateTime , inicio : Int , fin : Int){
-	  if(this.sePuedeReservar(dia, inicio, fin)){
-	    this.reservas+= new Reserva(this,dia,inicio,fin)
-	  }
+	  this.sePuedeReservar(dia, inicio, fin)
+	  this.reservas+= new Reserva(this,dia,inicio,fin)
 	  println(this.precioFinal(dia, inicio, fin, this.precioBase))
 	}
 	
-	private def sePuedeReservar(dia : DateTime, inicio : Int, fin : Int) : Boolean ={
+	private def chequearSiPuedeReservar(dia : DateTime, inicio : Int, fin : Int) : Boolean ={
 	  if(hayReserva(dia,inicio,fin)){
 	    new YaEstaReservada()
 	  }
 	  if(this.esDeNoche(inicio) & ! this.tieneIluminacion){
 	      new NoTieneIluminacion
 	  }
-	  true
 	}
 	
 	private def hayReserva(dia : DateTime, inicio : Int, fin : Int): Boolean = {
 		reservas.exists{r => r.dia.equals(dia) & r.inicio == inicio & r.fin == fin }
 	}
 	
-	private def tieneIluminacion() = false
+	def tieneIluminacion() = false
 	
 	private def esDeNoche(horaInicio : Int) = (horaInicio >= 18)
 	
-	def precioBase() : Double = 0.0
+	def precioBase() : Double 
 	
 	
-	def precioFinal(dia : DateTime, inicio : Int, fin : Int, pb : Double) : Double = 0
+	def precioFinal(dia : DateTime, inicio : Int, fin : Int, pb : Double) : Double 
 	
 	def calcularExtra(dia : DateTime, inicio : Int, fin : Int, pb : Double) : Double = 0
 }
