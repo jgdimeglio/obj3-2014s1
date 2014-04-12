a8d0a883b9fa3349659dbd16d8817eafb9a768af
unknown <jgdimeglio@gmail.com>
2014-04-12 23:44:42 +0200

    Se arreglo el metodo hayReserva

---
+++ complejo-deportivo/src/main/java/canchas/Cancha.scala
@@ -0,0 +1,45 @@
+package Clases
+
+import org.joda.time.DateTime
+
+import scala.collection.mutable.ArrayBuffer
+
+import Excepciones.NoTieneIluminacion
+import Excepciones.YaEstaReservada
+
+abstract class Cancha() {
+	val reservas = new ArrayBuffer[Reserva]
+
+
+	def reservar(dia : DateTime , inicio : Int , fin : Int){
+	  this.chequearSiPuedeReservar(dia, inicio, fin)
+	  this.reservas+= new Reserva(this,dia,inicio,fin)
+	  println(this.precioFinal(dia, inicio, fin))
+	}
+
+	private def chequearSiPuedeReservar(dia : DateTime, inicio : Int, fin : Int) ={
+	  if(hayReserva(dia,inicio,fin)){
+	    new YaEstaReservada()
+	  }
+	  if(this.esDeNoche(inicio) & ! this.tieneIluminacion){
+	      new NoTieneIluminacion
+	  }
+	}
+
+	private def hayReserva(dia : DateTime, inicio : Int, fin : Int): Boolean = {
+		reservas.exists{r => r.dia.equals(dia) & (r.inicio <= inicio & r.inicio >= fin) | (r.fin <= inicio & r.fin >= fin) }
+	}
+
+	def tieneIluminacion() = false
+
+	private def esDeNoche(horaInicio : Int) = (horaInicio >= 18)
+
+	def precioBase() : Double 
+
+
+	def precioFinal(dia : DateTime, inicio : Int, fin : Int) : Double ={
+	  this.precioBase() + this.calcularExtra(dia, inicio, fin, this.precioBase)
+	}
+
+	def calcularExtra(dia : DateTime, inicio : Int, fin : Int, pb : Double) : Double = 0
+}
