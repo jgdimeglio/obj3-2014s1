b1c6cfa8feb60f0266c976f71dfce93b8cdc62ef
José Di Meglio <jgdimeglio@gmail.com>
2014-04-13 01:24:45 +0200

    override olvidado

--- complejo-deportivo/src/main/java/Mixins/Iluminacion.scala
+++ complejo-deportivo/src/main/java/Mixins/Iluminacion.scala
@@ -5,7 +5,7 @@ import org.joda.time.DateTime
 
 trait Iluminacion extends Caracteristica {
 
-   def extraPorCaracteristica(pb: Double) = (0.2 * pb)
+   override def extraPorCaracteristica(pb: Double) = (0.2 * pb)
    
    override def tieneIluminacion() = true
 }
