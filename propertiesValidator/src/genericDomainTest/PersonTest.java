487ab43b702b47ca6d65bd8677a63147b1883fff
José Di Meglio <jgdimeglio@gmail.com>
2014-05-17 22:15:08 +0200

    descomentando el fail

--- propertiesValidator/src/genericDomainTest/PersonTest.java
+++ propertiesValidator/src/genericDomainTest/PersonTest.java
@@ -1,6 +1,7 @@
 package genericDomainTest;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import exceptions.InvalidValueException;
 import genericDomain.Person;
 import genericDomain.ValidateStringNotEmptyException;
@@ -14,13 +15,13 @@ public class PersonTest {
 	
 	@Test
     public void noDebePermitirNombresVacios () {
-		Person p1 = new Person();
+	Person p1 = new Person();
         p1.addValidator( "name", new ValidateStringNotEmptyException());
         p1.setName("Nico");
         assertEquals("Nico", p1.getName()); // Se asigna el nombre correctamente
         try {
             p1.setName("");
-            //fail("Debera haber tirado excepcion al asignarle un nombre vacio);
+            fail("Debera haber tirado excepcion al asignarle un nombre vacio");
         }
         catch (InvalidValueException e) {
             assertEquals("Nico", p1.getName()); // El nombre no cambio
